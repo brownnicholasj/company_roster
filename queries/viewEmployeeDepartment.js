b88e647dfd13f3dff69efe11cac6c82d1710eb5d
brownnicholasj <brown.nicholasj@gmail.com>
2021-06-04 08:19:16 -0500

    update query to add manager to dept view

--- queries/viewEmployeeDepartment.js
+++ queries/viewEmployeeDepartment.js
@@ -13,7 +13,7 @@ const conn = mysql.createConnection({
 class ViewEmployeeDepartment {
 	constructor(selection) {
 		conn.query(
-			`SELECT d.name AS Dept, e.id,e.first_name,e.last_name,r.title FROM employee AS e LEFT JOIN role AS r ON e.role_id=r.role_id LEFT JOIN department AS d ON r.department_id=d.department_id WHERE d.name = '${selection}'`,
+			`SELECT d.name AS department, e.id,e.first_name,e.last_name,r.title, CONCAT(m.first_name,' ', m.last_name) AS manager FROM employee AS e LEFT JOIN role AS r ON e.role_id=r.role_id LEFT JOIN department AS d ON r.department_id=d.department_id LEFT JOIN employee AS m ON e.manager_id=m.id WHERE d.name = '${selection}'`,
 			(err, res) => {
 				if (err) throw err;
 				console.table(res);
